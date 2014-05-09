dfb3a025c620f00bfa3dc9bb99f5f355e6e43038
Nicholas Bruning <nicholas@bruning.com.au>
2014-05-09 12:37:33 +1000

    Release v0.1.5

--- build/fishpond-client.js
+++ build/fishpond-client.js
@@ -1,4 +1,4 @@
-/*! fishpond-client v0.1.4 | 2014-05-09 */
+/*! fishpond-client v0.1.5 | 2014-05-09 */
 //----------------------------------
 // Taken from: http://stackoverflow.com/questions/2790001/fixing-javascript-array-functions-in-internet-explorer-indexof-foreach-etc
 //
